5e7763ae6369b381dafaab05a7e4869c37c33ec3
Embarca <alexandrosoares221@gmail.com>
2025-01-19 14:05:18 -0300

    Adição da função ligar Leds

--- tarefa_2.c
+++ tarefa_2.c
@@ -20,6 +20,40 @@ void desligar_leds () {
     gpio_put(pin_r, false);
 }
 
+// Função para acender os Leds individuais
+
+void acender_led_verde () {
+    // Acender LED verde
+    gpio_put(pin_g, 1);
+}  
+
+void acender_led_azul () {
+    // Acender LED azul
+    gpio_put(pin_b, 1);
+}   
+
+void acender_led_vermelho () {
+    // Acender LED vermelho
+    gpio_put(pin_r, 1);
+}   
+
+// Função para apagar os Leds individuais
+
+void apagar_led_verde () {
+    // Apagar LED verde
+    gpio_put(pin_g, 0);
+}  
+
+void apagar_led_azul () {
+    // Apagar LED azul
+    gpio_put(pin_b, 0);
+}   
+
+void apagar_led_vermelho () {
+    // Apagar LED vermelho
+    gpio_put(pin_r, 0);
+}   
+
 int main() {
     stdio_init_all(); // Inicializa as interfaces de entrada e saída
 
@@ -36,6 +70,13 @@ int main() {
         Utilizar as funções abaixo para ligar e desligar os LEDs.*/
         ligar_leds (); // Irá ligar todos os pinos (3), tomando a cor branca
         desligar_leds (); // Irá desligar todos os pinos (3), o LED apaga
+        acender_led_verde (); // Irá acender o Led Verde
+        acender_led_azul (); // Irá acender o Led Azul
+        acender_led_vermelho (); // Irá acender o Led Vermelho
+        apagar_led_verde (); // Irá apagar o Led Verde
+        apagar_led_azul (); // Irá apagar o Led Azul
+        apagar_led_vermelho (); // Irá apagar o Led Vermelho
+
     }
     
 }
