51f25125356068fd9e0dfb4b6f848e243a76ea30
Edna Rodrigues <ednards2002@gmail.com>
2025-01-19 00:39:21 -0300

    Correção de recuos.

--- tarefa_2.c
+++ tarefa_2.c
@@ -8,16 +8,16 @@
 
 // Função para ligar todos os LEDs
 void ligar_todos () {
-  gpio_put(pin_g, true);
-  gpio_put(pin_b, true);
-  gpio_put(pin_r, true);
+    gpio_put(pin_g, true);
+    gpio_put(pin_b, true);
+    gpio_put(pin_r, true);
 }
 
 // Função para desligar todos os LEDs
 void desligar_todos () {
-  gpio_put(pin_g, false);
-  gpio_put(pin_b, false);
-  gpio_put(pin_r, false);
+    gpio_put(pin_g, false);
+    gpio_put(pin_b, false);
+    gpio_put(pin_r, false);
 }
 
 int main() {
