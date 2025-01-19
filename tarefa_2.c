c65623ce1b2dfd92348afad38b9e1573fc110d60
Edna Rodrigues <ednards2002@gmail.com>
2025-01-19 00:23:49 -0300

    Declaração de pinos de leds e bibliotecas a serem usadas

---
+++ tarefa_2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+// Declaração de pinos de saídas dos LEDs
+#define pin_g 11 // LED verde
+#define pin_b 12 // LED azul
+#define pin_r 13 // LED vermelho
+
+int main() {
+    stdio_init_all();
+}
